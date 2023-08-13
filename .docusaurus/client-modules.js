574afbd18d16967482951413b462dd27c78f084e
xebec19 <rohant946@gmail.com>
2023-08-13 22:55:11 +0530

    feat: setup docusauras

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/rohan/Code/web/blog/node_modules/infima/dist/css/default/default.css'),
+  require('/home/rohan/Code/web/blog/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/rohan/Code/web/blog/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/rohan/Code/web/blog/src/css/custom.css'),
+];
---
+++ .docusaurus/routes.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '1d8'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '55a'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', '261'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', '536'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', 'd3a'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '34e'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '856'),
+    exact: true
+  },
+  {
+    path: '/blog',
+    component: ComponentCreator('/blog', 'f25'),
+    exact: true
+  },
+  {
+    path: '/blog/archive',
+    component: ComponentCreator('/blog/archive', '089'),
+    exact: true
+  },
+  {
+    path: '/blog/html-sse',
+    component: ComponentCreator('/blog/html-sse', 'f6e'),
+    exact: true
+  },
+  {
+    path: '/markdown-page',
+    component: ComponentCreator('/markdown-page', '3df'),
+    exact: true
+  },
+  {
+    path: '/docs',
+    component: ComponentCreator('/docs', '87d'),
+    routes: [
+      {
+        path: '/docs/category/go',
+        component: ComponentCreator('/docs/category/go', '685'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/docs/go/database-migration-in-go-with-soda',
+        component: ComponentCreator('/docs/go/database-migration-in-go-with-soda', '9a0'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      }
+    ]
+  },
+  {
+    path: '/',
+    component: ComponentCreator('/', 'a1d'),
+    exact: true
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*'),
+  },
+];
