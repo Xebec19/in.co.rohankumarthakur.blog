25f38701c6eb8f8a6ff9bfa6458f9e8a4645aca0
xebec19 <rohant946@gmail.com>
2023-08-15 22:45:46 +0530

    refactor: deleted .docusauras folder and updated home page

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/rohan/Code/web/blog/node_modules/infima/dist/css/default/default.css'),
-  require('/home/rohan/Code/web/blog/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/rohan/Code/web/blog/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/rohan/Code/web/blog/src/css/custom.css'),
-];
