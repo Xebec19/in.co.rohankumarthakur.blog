1c060f1f69200e067916aac7eebb10b94fa5fa17
xebec19 <rohant946@gmail.com>
2023-08-26 19:49:44 +0530

    feat(config): added social preview image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,7 +94,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/blog-social-card.png",
       navbar: {
         title: "My Site",
         logo: {
